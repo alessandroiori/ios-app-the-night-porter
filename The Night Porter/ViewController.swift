96a26b62b1721e00d5c7088f6064182a72b53988
alessandroiori <alessandro.iori92@gmail.com>
2020-05-02 01:57:51 +0200

    added dark mode button

--- The Night Porter/ViewController.swift
+++ The Night Porter/ViewController.swift
@@ -6,8 +6,9 @@
 //  Copyright © 2020 Alessandro Iori. All rights reserved.
 //
 
-import UIKit
+import UIKit // core network for any iOS application
 
+// View is anything that is cabable of drawing itself in a rectangular region on iOS screen. View object: Label, button, img, bar ecc
 class ViewController: UIViewController {
 
     override func viewDidLoad() {
@@ -16,8 +17,33 @@ class ViewController: UIViewController {
     }
 
     @IBAction func changeBackground(_ sender: Any) {
+        // "view" acts as container of the application, top-level view object that contains subviews: Label, button ecc
         view.backgroundColor = UIColor.darkGray
+        
+        
+        // array of subview (unwrap optional)
+        let everything = view.subviews
+        
+        for eachView in everything {
+            // is it a label? (not button)
+            if eachView is UILabel {
+                // downcast as UILabel from UIView
+                let currentLabel = eachView as! UILabel
+                currentLabel.textColor = UIColor.lightGray
+            }
+        }
+        
+        /*
+        // compact way
+        let UILabelOnly = view.subviews
+            .filter { $0 is UILabel } // is it a label? (not button)
+            .map { $0 as! UILabel} // downcast as UILabel from UIView
+        
+        for label in UILabelOnly {
+            label.textColor = UIColor.lightGray
+        }
+        */
+        
     }
-    
 }
 
