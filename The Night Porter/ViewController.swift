8b752ff6d7e48c683efb2b4650236977172bd0de
alessandroiori <alessandro.iori92@gmail.com>
2020-05-01 18:50:51 +0200

    init project

---
+++ The Night Porter/ViewController.swift
@@ -0,0 +1,23 @@
+//
+//  ViewController.swift
+//  The Night Porter
+//
+//  Created by Alessandro Iori on 01/05/2020.
+//  Copyright © 2020 Alessandro Iori. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+    @IBAction func changeBackground(_ sender: Any) {
+        view.backgroundColor = UIColor.darkGray
+    }
+    
+}
+
